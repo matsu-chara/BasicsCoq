f21ebf82bf5f304c5fa4e81ec5e8512b1557f575
matsu-chara <matsuy00@gmail.com>
2015-06-12 01:52:43 +0900

    factorial

--- Basics_J/Basics.v
+++ Basics_J/Basics.v
@@ -87,10 +87,84 @@ Proof. simpl. reflexivity. Qed.
 Check (negb).
 
 Module Playground1.
+  Inductive nat : Type :=
+  | O: nat
+  | S: nat -> nat.
 
-  Inductive nat: Type :=
-    | O: nat
-    | S: nat.
-            
-             
-end Playground1.
+  Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+End Playground1.
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
+
+Check (S(S(S(S O)))).
+Eval simpl in (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) :bool :=
+  match n with
+    | O => true
+    | S O => false
+    | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat): bool := negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. simpl. reflexivity. Qed.
+
+Module Playground2.
+  Fixpoint plus (n: nat)(m: nat):nat :=
+    match n with
+      | O => m
+      | S n' => S (plus n' m)
+    end.
+
+  Eval simpl in (plus (S (S (S O))) (S (S O))).
+
+  Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+
+  Fixpoint minus (n m:nat) : nat :=
+  match n, m with
+  | O , _ => O
+  | S _ , O => n
+  | S n', S m' => minus n' m'
+  end.
+
+  
+End Playground2.
+
+Fixpoint exp (base power : nat) : nat :=
+  match power with
+    | O => S O
+    | S p => mult base (exp base p)
+  end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint factorial (n:nat) : nat :=
+  match n with
+    | O => 1
+    | S n'  => mult n (factorial n')
+  end.
+
+Example test_factorial1: (factorial 3) = 6.
+Example test_factorial2: (factorial 5) = (mult 10 12).
